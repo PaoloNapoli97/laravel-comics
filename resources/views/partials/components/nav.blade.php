850606920eb310748b40ad65c1fc5f49f696288e
PaoloNapoli97 <paolonapoli@live.it>
2023-01-14 17:28:23 +0100

    css non funziona, ho passato 5 ore, ci rinuncio

---
+++ resources/views/partials/components/nav.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="powered"></div>
+    <div class="nav">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>
+        <nav class="main-nav">
+            <ul>
+                <li>
+                    ciao
+                </li>
+                <li>
+                    ciao
+                </li>
+                <li>
+                    ciao
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
